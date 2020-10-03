d88a6feda1e9b32aa318d4990173f2a3c28de35c
David <daveski084@users.noreply.github.com>
2020-10-03 12:52:35 -0400

    Start Button behaviour
    Added script to start button to change scenes.

---
+++ Assets/Scripts/StartButtonBehaviourScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButtonBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnstartButtonPressed()
+    {
+        Debug.Log("Start Button Pressed.");
+        SceneManager.LoadScene("MainMenu");
+    }
+}
