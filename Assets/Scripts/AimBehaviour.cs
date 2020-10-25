f5d0103ced2c788bb1f8c89e7f0fa507e5bbbfbb
David <daveski084@users.noreply.github.com>
2020-10-24 22:28:54 -0400

    Added animations

---
+++ Assets/Scripts/AimBehaviour.cs
@@ -0,0 +1,40 @@
+﻿/***********************************************************************;
+* Project            : Shiba Scramble
+*
+* Program name       : "PlayButtonBehaviourScript.cs"
+*
+* Author             : David Gasinec
+* 
+* Student Number     : 101187910
+*
+* Date created       : 20/10/22
+*
+* Description        : Load scenes with button press.
+*
+* Last modified      : 20/10/03
+*
+* Revision History   :
+*
+*Date        Author Ref    Revision (Date in YYYYMMDD format) 
+*201003    David Gasinec        Created script. 
+*
+|**********************************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
